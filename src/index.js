5e7d47633ed61c6192fba44b722285b9f7619660
HanadAlly <allyhanad404@gmail.com>
2025-03-24 09:06:47 +0300

    commit changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 // Your code here
+document.addEventListener("DOMContentLoaded", () => {
+    const characterBar = document.getElementById("character-bar");
+    const detailedInfo = document.getElementById("detailed-info");
+    const voteForm = document.getElementById("votes-form");
+    const resetButton = document.getElementById("reset-btn");
+
+  
